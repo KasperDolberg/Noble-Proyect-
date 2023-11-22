b15abf277ba0ba7efa862531bc18c77dea656690
KevinVillalba <kevinvillalba22222@gmail.com>
2023-11-22 18:35:37 -0300

    se finaliza la creacion de la section5

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Nav from '/src/components/global/Nav.jsx'
 import Section1 from '/src/components/home/Section1'
+import Section5 from '/src/components/home/Section5'
 
 function App() {
   return (
     <>
     <Nav/>
     <Section1/> 
+    <Section5/>
     </>
   )
 }
---
+++ @latest/src/components/home/Section5.jsx
@@ -0,0 +1,123 @@
+import React from 'react'
+import '/src/stylesheets/Section5.css';
+
+const Section5 = () => {
+  return (
+    <section>
+      <div className="container-father">
+        <div className="container-secondary">
+
+        <div className="cards-container">
+
+            <div className="card-about">
+            <div className="image-container">
+               <img src="/src/img/company.jpg" alt="" />
+            </div> 
+              <div className="title-container-father">
+                <div className="title-container">
+                  <h2>Learn more about Noble</h2>
+                  <div className="css-17otgo1">
+                    <button
+                      data-focus-spacing="4"
+                      id="tabletDesktop-5414661"
+                      label="Book a test drive"
+                      target="_blank"
+                      rel="noreferrer noopener"
+                      href="/us/test-drive/booking/ps2/"
+                      className="css-ttcv04"
+                    >
+                      <span className="css-1j2esho">
+                        <span className="css-1lfoa71">About us</span>
+                        <span
+                          aria-hidden="true"
+                          data-type="icon"
+                          className="css-d4b8st"
+                        >
+                          <div className="css-1ckl8y4">
+                            <div
+                              className="css-jyc14k"
+                              style={{ opacity: 1, transform: 'translateX(0em)' }}
+                            >
+                              <svg
+                                viewBox="0 0 16 16"
+                                fill="none"
+                                height="1em"
+                                role="img"
+                              >
+                                <path
+                                  d="M8 2 6.95 3.05l4.2 4.2H2v1.5h9.15l-4.2 4.2L8 14l6-6-6-6Z"
+                                  fill="currentColor"
+                                ></path>
+                              </svg>
+                            </div>
+                          </div>
+                        </span>
+                      </span>
+                    </button>
+                  </div>
+                </div> 
+              </div>
+            </div>
+
+
+            <div className="card-news">
+              <div className="image-container">
+                <img src="./src/img/news-1-.jpg" alt="" />
+              </div> 
+              <div className="title-container-father">
+                <div className="title-container">
+                <h2>Stay up to date of our news</h2>
+                <div className="css-17otgo1">
+                  <button
+                    data-focus-spacing="4"
+                    id="tabletDesktop-5414661"
+                    label="Book a test drive"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    href="/us/test-drive/booking/ps2/"
+                    className="css-ttcv04"
+                  >
+                    <span className="css-1j2esho">
+                      <span className="css-1lfoa71">View News</span>
+                      <span
+                        aria-hidden="true"
+                        data-type="icon"
+                        className="css-d4b8st"
+                      >
+                        <div className="css-1ckl8y4">
+                          <div
+                            className="css-jyc14k"
+                            style={{ opacity: 1, transform: 'translateX(0em)' }}
+                          >
+                            <svg
+                              viewBox="0 0 16 16"
+                              fill="none"
+                              height="1em"
+                              role="img"
+                            >
+                              <path
+                                d="M8 2 6.95 3.05l4.2 4.2H2v1.5h9.15l-4.2 4.2L8 14l6-6-6-6Z"
+                                fill="currentColor"
+                              ></path>
+                            </svg>
+                          </div>
+                        </div>
+                      </span>
+                    </span>
+                  </button>
+                </div>
+              </div>
+              </div>
+            </div>
+
+        </div>
+
+      </div>
+      </div>
+
+
+    </section>
+  )
+}
+
+export default Section5
