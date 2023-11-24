130e3fa005873f5d78ff154ca145baa54da51d74
Lautaro Ayala <nicolelautarole@gmal.com>
2023-11-24 10:22:56 -0300

    Section2 agregada

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Nav from '/src/components/global/Nav.jsx'
 import Section1 from '/src/components/home/Section1'
+import Section2 from './components/home/section2'
 import Section5 from '/src/components/home/Section5'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
     <Nav/>
     <Section1/> 
+    <Section2/> 
     <Section5/>
     </>
   )
