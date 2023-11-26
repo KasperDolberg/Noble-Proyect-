9bb4904e00cd9813c610ceb8c12be3f296a7a1c4
cristian gavilan <cristianeldorado4@gmail.com>
2023-11-26 10:12:13 -0300

    Footer finalizado

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import Nav from '/src/components/global/Nav.jsx'
-import Section1 from '/src/components/home/Section1'
-import Section2 from './components/home/section2'
-import Section5 from '/src/components/home/Section5'
+import React from "react";
+import Nav from "/src/components/global/Nav.jsx";
+import Footer from "/src/components/global/Footer.jsx";
+import Section1 from "/src/components/home/Section1";
+import Section2 from "./components/home/section2";
+import Section5 from "/src/components/home/Section5";
 
 function App() {
   return (
     <>
-    <Nav/>
-    <Section1/> 
-    <Section2/> 
-    <Section5/>
+      <Nav />
+      <Section1 />
+      <Section2 />
+      <Section5 />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
